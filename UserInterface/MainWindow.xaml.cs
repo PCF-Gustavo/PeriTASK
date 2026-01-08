8c78392638dbf144b4fa6079dd9ddf9528586317
Gustavo Vieira da Silva <gustavo.gvs@pf.gov.br>
2026-01-07 23:33:40 -0300

    ok

--- UserInterface/MainWindow.xaml.cs
+++ UserInterface/MainWindow.xaml.cs
@@ -9,27 +9,27 @@ namespace UserInterface
 {
     public partial class MainWindow : Window
     {
-        private List<string> _arquivosSelecionados;
+        private List<string> arquivos_selecionados;
 
         public MainWindow()
         {
             InitializeComponent();
 
             // pega os argumentos (ignora o exe)
-            _arquivosSelecionados = Environment.GetCommandLineArgs()
+            arquivos_selecionados = Environment.GetCommandLineArgs()
                                                .Skip(1)
                                                .ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (_arquivosSelecionados == null || _arquivosSelecionados.Count == 0)
+            if (arquivos_selecionados == null || arquivos_selecionados.Count == 0)
             {
                 MessageBox.Show("Nenhum arquivo recebido.");
                 return;
             }
 
-            ExecutarPython(_arquivosSelecionados);
+            ExecutarPython(arquivos_selecionados);
 
             this.Close();
         }
@@ -52,14 +52,6 @@ namespace UserInterface
             };
 
             using var process = Process.Start(psi);
-            //string resultado = process.StandardOutput.ReadToEnd();
-            //process.WaitForExit();
-
-            // opcional: mostrar onde o txt foi criado
-            //if (!string.IsNullOrWhiteSpace(resultado))
-            //{
-            //    MessageBox.Show($"Arquivo criado em:\n{resultado.Trim()}");
-            //}
         }
     }
 }
